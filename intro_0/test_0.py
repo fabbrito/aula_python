3de51dbb30998ec9a8529ac47193e647c6826022
Fabrício <fab.brito94@gmail.com>
2019-09-20 18:57:42 -0300

    Aula intro 0

---
+++ intro_0/test_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+with open('texto.txt', 'r') as p_arquivo:
+    t = p_arquivo.read()
+
+print(repr(t))
+
+t = t[:-1] + ' MUITO NOOB\n'
+
+print(t)
+
+with open('texto_mod.txt', 'a') as p_arquivo:
+    p_arquivo.write(t)
